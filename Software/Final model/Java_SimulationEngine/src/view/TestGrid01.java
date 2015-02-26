733a4c1c70045ac668f55f30dab83e8566718758
zakimoinuddin <zakimoinuddinster@gmail.com>
2015-02-26 17:26:40 +0000

    Map testing

--- Software/Final model/Java_SimulationEngine/src/view/TestGrid01.java
+++ Software/Final model/Java_SimulationEngine/src/view/TestGrid01.java
@@ -12,13 +12,21 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.ImageCapabilities;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
@@ -32,6 +40,16 @@ public class TestGrid01 {
 	private IGrid nGrid;
 	private int gHeight;
 	private int gWidth;
+	Image grassImg;
+	Image roadImg;
+	
+	
+	Image grass = new ImageIcon("images/0.png").getImage();
+	
+	/** The road. */
+	Image road = new ImageIcon("images/1.png").getImage();
+	
+	
 
     public TestGrid01(IGrid grid) {
     	
@@ -53,6 +71,8 @@ public class TestGrid01 {
                 JFrame frame = new JFrame("Testing");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setLayout(new BorderLayout());
+                frame.setSize(250, 400);
+                
                 frame.add(new TestPane());
                 frame.pack();
                 frame.setLocationRelativeTo(null);
@@ -67,24 +87,28 @@ public class TestGrid01 {
     	 * for deciding the height and width values of the cell.
     	 */
     	
-        private int columnCount = 10;
-        private int rowCount = 10;
+      //  private int columnCount = 10;
+       // private int rowCount = 10;
         
         //Rectangular Cells
         //Now i have to draw the cells based on the 
-        private List<Rectangle> cells;
+       // private List<Rectangle> cells;
         
         /*Used instead of a Car just to get a look and feel about how to have a control
          * of how the Cell works by just moving the mouse
          * 
          * 
          */
-        private Point selectedCell;
+      //  private Point selectedCell;
 
         public TestPane() {
-        	//put all the cells column*row into an ArrayList
-        	cells = new ArrayList<>(gHeight * gWidth);
         	
+        	
+        	
+        	
+        	//put all the cells column*row into an ArrayList
+        	//cells = new ArrayList<>(gHeight * gWidth);
+        /*	
         	//Mouse Adapter class methods to get awareness of the kind of 
             MouseAdapter mouseHandler = new MouseAdapter() {
                 @Override
@@ -101,7 +125,7 @@ public class TestGrid01 {
                     int row = e.getY() / cellHeight;
                     
 
-                    selectedCell = new Point(column, row);
+                    selectedCell = new Point(row,column);
                     repaint();
 
                 }
@@ -112,7 +136,7 @@ public class TestGrid01 {
         @Override
         //Dimension of the Parent Component
         public Dimension getPreferredSize() {
-            return new Dimension(600, 600);
+            return new Dimension(1000, 1000);
         }
         //invalidate the cells not required but, just as a Good Coding practice.
         @Override
@@ -127,6 +151,20 @@ public class TestGrid01 {
         
         	super.paintComponent(g);
             Graphics2D g2d = (Graphics2D) g.create();
+            int cellCount=0;
+            try{
+            	//Is used to fetch the grass image
+                File inGrass = new File("./files/grass2-20.png");
+                grassImg = ImageIO.read(inGrass);
+                
+                //uSed to fetch the road image
+                File inRoad = new File("./files/road20.png");
+                roadImg = ImageIO.read(inRoad);
+                }
+                catch(IOException ie)
+                {
+                    System.out.println(ie.getMessage());
+                }
             
             //to get the width and height of the component
             //25/4 not required.
@@ -146,35 +184,73 @@ public class TestGrid01 {
             
             // Good coding practice just to clear if any previous traces remain in the junk
             if (cells.isEmpty()) {
-                for (int row = 0; row < gWidth ; row++) {
-                    for (int col = 0; col < gHeight; col++) {
+                for (int row = 0; row < gHeight ; row++) {
+                    for (int col = 0; col < gWidth; col++) {
                     	
                     	
-                    	if(nGrid.getCellType(row, col)== CellType.EMPTY){
+        /*           	if(nGrid.getCellType(row, col)== CellType.EMPTY){
                     		Rectangle cell = new Rectangle(
-                                    xOffset + (row * cellWidth),
-                                    yOffset + (col * cellHeight),
-                                    cellWidth,
-                                    cellHeight);
+                                    xOffset + (row * cellHeight),
+                                    yOffset + (col * cellWidth),
+                                    cellHeight,
+                                    cellWidth);
                     		
                             cells.add(cell);
+                            cellCount++;
                     		
                     	}
                     	else if(nGrid.getCellType(row, col)== CellType.ROAD){
                     		
                     		Rectangle cell = new Rectangle(
-                                    xOffset + (row * cellWidth),
-                                    yOffset + (col * cellHeight),
-                                    cellWidth,
-                                    cellHeight);
+                    				 xOffset + (row * cellHeight),
+                                     yOffset + (col * cellWidth),
+                                     cellHeight,
+                                     cellWidth);
                     		
                             cells.add(cell);
+                            cellCount++;
+                    	}
+        	
+        	
+        	
+                    	/////////////////////////////
+        	
+        	
+        	
+                     if(nGrid.getCellType(row, col)== CellType.EMPTY){
+                    		
+                    	 Rectangle cell = new Rectangle(
+                				 xOffset + (row * cellHeight),
+                                 yOffset + (col * cellWidth),
+                                 cellHeight,
+                                 cellWidth);
+                     
+                     		//g.drawImage(grassImg, row, col, xOffset + (row * cellHeight),  yOffset + (col * cellWidth),null);
+                     		
+                        //    cells.add(g.drawImage(grassImg, row, col, xOffset + (row * cellHeight),  yOffset + (col * cellWidth),null));
+                            cellCount++;
+                    		
+                    	}
+                     	else if(nGrid.getCellType(row, col)== CellType.ROAD){
+                    		
+                    		Rectangle cell = new Rectangle(
+                    				 xOffset + (row * cellHeight),
+                                     yOffset + (col * cellWidth),
+                                     cellHeight,
+                                     cellWidth);
+                    		
+                    		
+                          //  cells.add(cell);
+                            cellCount++;
                     	}
                     	
                     	
-                        
+                    	System.out.println("The no of Column are: "+col);   
                     }
+                    System.out.println("The no of rows are: "+row);
+                    
                 }
+                System.out.println("The No of cells is: "+cellCount);
             }
 
             if (selectedCell != null) {
@@ -182,6 +258,7 @@ public class TestGrid01 {
                 int index = selectedCell.x + (selectedCell.y * columnCount);
                 Rectangle cell = cells.get(index);
                 g2d.setColor(Color.BLUE);
+               // Shape sh = (Shape)cell;
                 g2d.fill(cell);
 
             }
@@ -189,9 +266,48 @@ public class TestGrid01 {
             g2d.setColor(Color.GRAY);
             for (Rectangle cell : cells) {
                 g2d.draw(cell);
+               // g2d.drawImage(cell,null,null);
             }
 
             g2d.dispose();
-        }
+        }*/
     }
+        public void paint (Graphics g) {
+    		g=(Graphics2D) g;
+    		
+    		
+    		
+    		int px=0;
+    		int py=0;
+    		
+    	for(int x=0; x<40; x++)
+    	
+    		{
+    				for(int y=0; y<25; y++)
+    			{
+    				if(nGrid.getCellType(x, y)==CellType.EMPTY){
+    		
+    				px=x*10;
+					py=y*10;
+					g.drawImage(grass, px, py, null);
+    			
+    				}
+    				
+    				if(nGrid.getCellType(x, y)==CellType.ROAD){
+    		    		
+        				px=x*10;
+    					py=y*10;
+    					g.drawImage(road, px, py, null);
+        			
+        				}
+    				
+    				
+    			}
+    			
+    		
+    		}
+        
 }
+    
+
+}}
