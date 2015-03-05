a292dd228f840836081de0f3fec2d9fb2c7bbe13
zavr <k1463894@kcl.ac.uk>
2015-03-05 16:02:56 +0000

    Add new DataEvent type

--- Software/Final model/Java_SimulationEngine/src/events/EventDispatchable.java
+++ Software/Final model/Java_SimulationEngine/src/events/EventDispatchable.java
@@ -4,21 +4,31 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public abstract class EventDispatchable {
-	  private ArrayList<ModelEventListener> _listeners = new ArrayList<ModelEventListener>();
-	  public synchronized void addEventListener(ModelEventListener listener)  {
+	  private ArrayList<EventListener> _listeners = new ArrayList<EventListener>();
+	  public synchronized void addEventListener(EventListener listener)  {
 	    _listeners.add(listener);
 	  }
-	  public synchronized void removeEventListener(ModelEventListener listener)   {
+	  public synchronized void removeEventListener(EventListener listener)   {
 	    _listeners.remove(listener);
 	  }
 	 
 	  // call this method whenever you want to notify
 	  //the event listeners of the particular event
 	  public synchronized void fireEvent() {
-	    ModelEvent event = new ModelEvent(this);
-	    Iterator<ModelEventListener> i = _listeners.iterator();
+	    SimulationEvent event = new SimulationEvent(this);
+	    Iterator<EventListener> i = _listeners.iterator();
 	    while(i.hasNext())  {
-	      i.next().handleModelEvent(event);
+	      i.next().handleSimulationEvent(event);
 	    }
 	  }
+	  
+	  public synchronized void fireDataEvent(DataEvent de) {
+	    Iterator<EventListener> i = _listeners.iterator();
+	    while(i.hasNext())  {
+	      i.next().handleSimulationEvent(de);
+	    }
+	  }
+	  
+
+	  
 	}
--- Software/Final model/Java_SimulationEngine/src/events/ModelEventListener.java
+++
@@ -1,7 +0,0 @@
-package events;
-
-import java.util.EventObject;
-
-public interface ModelEventListener {
-	public void handleModelEvent(ModelEvent e);
-}
