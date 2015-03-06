f0f5f1bf151a425f2ce0d65115721fefba782e4e
z-vr <k1463894@kcl.ac.uk>
2015-03-06 18:08:54 +0000

    Add TrafficLight and TrafficLightColour classes

---
+++ Software/Final model/Java_SimulationEngine/src/core/TrafficLight.java
@@ -0,0 +1,23 @@
+package core;
+
+
+/**
+ * TrafficLight implementation of ITrafficLight interface
+ * @author Anton
+ *
+ */
+public class TrafficLight implements ITrafficLight {
+
+	@Override
+	public void setColour(TrafficLightColour colour) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public TrafficLightColour getColour() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
