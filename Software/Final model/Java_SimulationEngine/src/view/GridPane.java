9d37dac32ab07f48215662d72c0df9ab2d32562e
zavr <k1463894@kcl.ac.uk>
2015-03-17 20:04:43 +0000

    Add JScrollBar support for GridPane

--- Software/Final model/Java_SimulationEngine/src/view/GridPane.java
+++ Software/Final model/Java_SimulationEngine/src/view/GridPane.java
@@ -1,10 +1,12 @@
 package view;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
+
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+
 import core.CellType;
 import core.Direction;
 import core.IGrid;
@@ -37,205 +39,33 @@ public class GridPane extends JComponent {
 
 	private IGrid nGrid;
 	
-    	
-    	//public class Map extends JPanel implements ActionListener, Runnable{
-    	/** The main timer. */
-    	//Timer mainTimer = new Timer(30, this);
-    	
-    	/*Grid Column and Row Count which is used to as a parameter for
-    	 * for deciding the height and width values of the cell.
-    	 */
-    	
-      //  private int columnCount = 10;
-       // private int rowCount = 10;
-        
-        //Rectangular Cells
-        //Now i have to draw the cells based on the 
-       // private List<Rectangle> cells;
-        
-        /*Used instead of a Car just to get a look and feel about how to have a control
-         * of how the Cell works by just moving the mouse
-         * 
-         * 
-         */
-      //  private Point selectedCell;
-
-        public GridPane() {
-        	
-        	
-        	//mainTimer.start();
-        	
-        	
-        	//put all the cells column*row into an ArrayList
-        	//cells = new ArrayList<>(gHeight * gWidth);
-        /*	
-        	//Mouse Adapter class methods to get awareness of the kind of 
-            MouseAdapter mouseHandler = new MouseAdapter() {
-                @Override
-                public void mouseMoved(MouseEvent e) {
-                    Point point = e.getPoint();
-
-                    int width = getWidth();
-                    int height = getHeight();
-
-                    int cellWidth = width / gWidth;
-                    int cellHeight = height /gHeight;
-
-                    int column = e.getX() / cellWidth; 
-                    int row = e.getY() / cellHeight;
-                    
-
-                    selectedCell = new Point(row,column);
-                    repaint();
-
-                }
-            };
-            addMouseMotionListener(mouseHandler);
-        }
-
+	private static int CELL_WIDTH = 10;
+	private static int CELL_HEIGHT = 10;
+	
+	private int comp_width;
+	private int comp_height;
+	
+//	@Override
+//	public int getWidth(){
+//		System.out.println("get grid pane width");
+//		return 0;
+//	}
+//	public int getHeight(){
+//		System.out.println("get grid pane height");
+//		return 0;
+//	}
+//	@Override
+//	public Dimension getSize(){
+//		System.out.println("get comp size");
+//		return new Dimension(300, 300);//comp_width*CELL_WIDTH, comp_height*CELL_HEIGHT);
+//	}
+//	
+    public GridPane(){
+    
+    };
+    
         @Override
-        //Dimension of the Parent Component
-        public Dimension getPreferredSize() {
-            return new Dimension(1000, 1000);
-        }
-        //invalidate the cells not required but, just as a Good Coding practice.
-        @Override
-        public void invalidate() {
-            cells.clear();
-            selectedCell = null;
-            super.invalidate();
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-        
-        	super.paintComponent(g);
-            Graphics2D g2d = (Graphics2D) g.create();
-            int cellCount=0;
-            try{
-            	//Is used to fetch the grass image
-                File inGrass = new File("./files/grass2-20.png");
-                grassImg = ImageIO.read(inGrass);
-                
-                //uSed to fetch the road image
-                File inRoad = new File("./files/road20.png");
-                roadImg = ImageIO.read(inRoad);
-                }
-                catch(IOException ie)
-                {
-                    System.out.println(ie.getMessage());
-                }
-            
-            //to get the width and height of the component
-            //25/4 not required.
-           int width = getWidth();
-           int height = getHeight();
-            
-            //based on the width and height of the outer Component
-            
-            int cellWidth = width / gWidth;
-            int cellHeight = height / gHeight;
-            
-            // why did i write this i have to figure out.
-            int xOffset = (width - (gWidth * cellWidth)) / 2;
-            int yOffset = (height - (gHeight * cellHeight)) / 2;
-            
-            
-            
-            // Good coding practice just to clear if any previous traces remain in the junk
-            if (cells.isEmpty()) {
-                for (int row = 0; row < gHeight ; row++) {
-                    for (int col = 0; col < gWidth; col++) {
-                    	
-                    	
-        /*           	if(nGrid.getCellType(row, col)== CellType.EMPTY){
-                    		Rectangle cell = new Rectangle(
-                                    xOffset + (row * cellHeight),
-                                    yOffset + (col * cellWidth),
-                                    cellHeight,
-                                    cellWidth);
-                    		
-                            cells.add(cell);
-                            cellCount++;
-                    		
-                    	}
-                    	else if(nGrid.getCellType(row, col)== CellType.ROAD){
-                    		
-                    		Rectangle cell = new Rectangle(
-                    				 xOffset + (row * cellHeight),
-                                     yOffset + (col * cellWidth),
-                                     cellHeight,
-                                     cellWidth);
-                    		
-                            cells.add(cell);
-                            cellCount++;
-                    	}
-        	
-        	
-        	
-                    	/////////////////////////////
-        	
-        	
-        	
-                     if(nGrid.getCellType(row, col)== CellType.EMPTY){
-                    		
-                    	 Rectangle cell = new Rectangle(
-                				 xOffset + (row * cellHeight),
-                                 yOffset + (col * cellWidth),
-                                 cellHeight,
-                                 cellWidth);
-                     
-                     		//g.drawImage(grassImg, row, col, xOffset + (row * cellHeight),  yOffset + (col * cellWidth),null);
-                     		
-                        //    cells.add(g.drawImage(grassImg, row, col, xOffset + (row * cellHeight),  yOffset + (col * cellWidth),null));
-                            cellCount++;
-                    		
-                    	}
-                     	else if(nGrid.getCellType(row, col)== CellType.ROAD){
-                    		
-                    		Rectangle cell = new Rectangle(
-                    				 xOffset + (row * cellHeight),
-                                     yOffset + (col * cellWidth),
-                                     cellHeight,
-                                     cellWidth);
-                    		
-                    		
-                          //  cells.add(cell);
-                            cellCount++;
-                    	}
-                    	
-                    	
-                    	System.out.println("The no of Column are: "+col);   
-                    }
-                    System.out.println("The no of rows are: "+row);
-                    
-                }
-                System.out.println("The No of cells is: "+cellCount);
-            }
-
-            if (selectedCell != null) {
-
-                int index = selectedCell.x + (selectedCell.y * columnCount);
-                Rectangle cell = cells.get(index);
-                g2d.setColor(Color.BLUE);
-               // Shape sh = (Shape)cell;
-                g2d.fill(cell);
-
-            }
-
-            g2d.setColor(Color.GRAY);
-            for (Rectangle cell : cells) {
-                g2d.draw(cell);
-               // g2d.drawImage(cell,null,null);
-            }
-
-            g2d.dispose();
-        }*/
-        	
-    }
-        @Override
-		public void paint (Graphics g) {
-    		g=g;
+		public void paintComponent (Graphics g) {
     		
     		int px=0;
     		int py=0;
@@ -244,8 +74,8 @@ public class GridPane extends JComponent {
     		{
 				for(int y=0; y<nGrid.getHeight(); y++)
 				{
-					px=x*10;
-					py=y*10;
+					px=x*CELL_WIDTH;
+					py=y*CELL_HEIGHT;
 						
     				if(getGrid().getCellType(x, y)==CellType.EMPTY){
 						g.drawImage(grass, px, py, null);
@@ -309,7 +139,6 @@ public class GridPane extends JComponent {
     			
     		
     		}
-        
         }
       
 		public IGrid getGrid() {
@@ -317,6 +146,9 @@ public class GridPane extends JComponent {
 		}
 		public void setGrid(IGrid nGrid) {
 			this.nGrid = nGrid;
+			Dimension d = new Dimension(nGrid.getWidth()*CELL_WIDTH, nGrid.getHeight()*CELL_HEIGHT);
+			this.setSize(d);
+			this.setPreferredSize(d);
 		}
     
 
--- Software/Final model/Java_SimulationEngine/src/view/TestGrid01.java
+++ Software/Final model/Java_SimulationEngine/src/view/TestGrid01.java
@@ -1,15 +1,21 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JViewport;
 import javax.swing.KeyStroke;
 
 import core.DataSimulator;
@@ -40,19 +46,70 @@ public class TestGrid01 implements EventListener {
 	private JMenuItem controlActionPause;
 	private JMenuItem dataActionStart;
 	private JMenuItem dataActionPause;
+	private JFrame frame;
+	private JLabel modelStatusLabel;
+	private JLabel dataStatusLabel;
 	
     public TestGrid01(Model model, DataSimulator ds) {
     	
     	this.model = model;
     	this.ds = ds;
     	
-        JFrame frame = new JFrame("«We-Got-This» Traffic Simulation Engine");
+        frame = new JFrame("«We-Got-This» Traffic Simulation Engine");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        frame.setSize(700, 500);
-        JMenuBar menuBar = new JMenuBar();
+        frame.setSize(new Dimension(750,500));
         
+        
+        setUpMenu();
+        
+        tp = new GridPane(); //simulation grid 
+        tp.setGrid(model.getGrid());        
+        
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.getViewport().add(tp);
+        JViewport vp = scrollPane.getViewport();
+        System.out.println(vp.getSize());
+        vp.setSize(tp.getWidth(), tp.getHeight());
+        System.out.println(vp.getSize());
+        
+        System.out.println("rect:"+tp.getWidth() + ":"+tp.getHeight());
+        
+        //System.out.println("width: "+tp.getSize().width);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        
+        scrollPane.setSize(200,200);
+        
+        frame.add(scrollPane);
+        
+        JPanel listPane = new JPanel();
+        listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
+        
+        modelStatusLabel = new JLabel("Model Status: x");
+        dataStatusLabel = new JLabel("Data Status: x");
+        
+        listPane.add(modelStatusLabel);
+        listPane.add(dataStatusLabel);
+        
+        frame.add(listPane, BorderLayout.LINE_END);
+        //frame.add(tp);
+        //frame.add(listPane);
+        
+        frame.setVisible(true);
+        
+        getCurrentData();
+
+    }
+    
+    private void getCurrentData(){
+    	setModelStatus(this.model.getStatus());
+        setDataStatus(this.ds.getRunning());
+    }
+    
+    private void setUpMenu(){
+    	JMenuBar menuBar = new JMenuBar();
         
         // Add the menubar to the frame
         frame.setJMenuBar(menuBar);
@@ -65,101 +122,59 @@ public class TestGrid01 implements EventListener {
         JMenu helpMenu = new JMenu("Help");
         
         //Managing Buttons 
-        JButton startButton = new JButton();
-   /*     try {
-       	    Image img = ImageIO.read(getClass().getResource("Start.png"));
-       	    startButton.setIcon(new ImageIcon(img)); 
-       	  } catch (IOException ex) {
-       	  }
-        startButton.setLocation(250, 20);
-        startButton.setSize(50,12);
+        //JButton startButton = new JButton();
         
+    	 menuBar.add(fileMenu);
+         menuBar.add(viewMenu);
+         menuBar.add(controlMenu);
+         menuBar.add(dataMenu);
+         menuBar.add(helpMenu);
+         
+         // Create and add simple menu item to one of the drop down menu
+         JMenuItem newAction = new JMenuItem("New");
+         JMenuItem openAction = new JMenuItem("Open");
+         JMenuItem exitAction = new JMenuItem("Exit");
+         JMenuItem cutAction = new JMenuItem("Cut");
+         JMenuItem copyAction = new JMenuItem("Copy");
+         JMenuItem pasteAction = new JMenuItem("Paste");
+         JMenuItem sizeAction = new JMenuItem("Adjust Size");
+         JMenuItem browserAction = new JMenuItem("Open in Browser");
         
-        JButton stopButton = new JButton("Stop Flow");
-        JButton restartButton = new JButton("Restart Flow");
-        startButton.setBackground(Color.GREEN);
-        stopButton.setBackground(Color.RED);
-        restartButton.setBackground(Color.GREEN);
-        
-        
-      Insets insets = contentPane.getInsets();
-        Dimension size = startButton.getPreferredSize();
-       startButton.setLocation(300,5);
-       startButton.setSize(40,10);
-        startButton.setBounds(25 + insets.left, 5 + insets.bottom,
-                     size.width, size.height);
-        size = stopButton.getPreferredSize();
-        stopButton.setBounds(55 + insets.left, 40 + insets.left,
-                     size.width, size.height);
-        size = restartButton.getPreferredSize();
-        restartButton.setBounds(150 + insets.left, 15 + insets.right,
-                     size.width + 50, size.height + 20);
-        
-        
-        contentPane.add(stopButton);
-        contentPane.add(restartButton); */
-        
-        menuBar.add(fileMenu);
-        menuBar.add(viewMenu);
-        menuBar.add(controlMenu);
-        menuBar.add(dataMenu);
-        menuBar.add(helpMenu);
-        
-        // Create and add simple menu item to one of the drop down menu
-        JMenuItem newAction = new JMenuItem("New");
-        JMenuItem openAction = new JMenuItem("Open");
-        JMenuItem exitAction = new JMenuItem("Exit");
-        JMenuItem cutAction = new JMenuItem("Cut");
-        JMenuItem copyAction = new JMenuItem("Copy");
-        JMenuItem pasteAction = new JMenuItem("Paste");
-        JMenuItem sizeAction = new JMenuItem("Adjust Size");
-        JMenuItem browserAction = new JMenuItem("Open in Browser");
+         controlActionStart = new JMenuItem("Start");
+         controlActionPause = new JMenuItem("Pause");
+         
+         dataActionStart = new JMenuItem("Start");
+         dataActionPause = new JMenuItem("Pause");
+         
+         JMenuItem helpAction = new JMenuItem("F1 for Help");
+         
+         controlActionStart.addActionListener(new MenuActionListener());
+         controlActionPause.addActionListener(new MenuActionListener());
+         
+         dataActionStart.addActionListener(new MenuActionListener());
+         dataActionPause.addActionListener(new MenuActionListener());
+         
+         controlActionStart.setAccelerator(KeyStroke.getKeyStroke('s'));
+         controlActionPause.setAccelerator(KeyStroke.getKeyStroke('p'));
+         
+         dataActionStart.setAccelerator(KeyStroke.getKeyStroke('S'));
+         dataActionPause.setAccelerator(KeyStroke.getKeyStroke('P'));
+
        
-        controlActionStart = new JMenuItem("Start");
-        controlActionPause = new JMenuItem("Pause");
-        
-        dataActionStart = new JMenuItem("Start");
-        dataActionPause = new JMenuItem("Pause");
-        
-        JMenuItem helpAction = new JMenuItem("F1 for Help");
-        
-        controlActionStart.addActionListener(new MenuActionListener());
-        controlActionPause.addActionListener(new MenuActionListener());
-        
-        dataActionStart.addActionListener(new MenuActionListener());
-        dataActionPause.addActionListener(new MenuActionListener());
-        
-        controlActionStart.setAccelerator(KeyStroke.getKeyStroke('s'));
-        controlActionPause.setAccelerator(KeyStroke.getKeyStroke('p'));
-        
-        dataActionStart.setAccelerator(KeyStroke.getKeyStroke('S'));
-        dataActionPause.setAccelerator(KeyStroke.getKeyStroke('P'));
-
-      
-        // Adding the respective menu with their respective Menu Items
-        fileMenu.add(newAction);
-        fileMenu.add(openAction);
-        fileMenu.add(cutAction);
-        fileMenu.add(copyAction);
-        fileMenu.add(pasteAction);
-        fileMenu.add(exitAction);
-        viewMenu.add(sizeAction);
-        viewMenu.add(browserAction);
-        controlMenu.add(controlActionStart);
-        controlMenu.add(controlActionPause);
-        dataMenu.add(dataActionStart);
-        dataMenu.add(dataActionPause);
-        helpMenu.add(helpAction);
-        
-        tp = new GridPane();
-        tp.setGrid(model.getGrid());                
-        frame.add(tp);
-   
-        frame.setVisible(true);
-        
-        setModelStatus(this.model.getStatus());
-        setDataStatus(this.ds.getRunning());
-
+         // Adding the respective menu with their respective Menu Items
+         fileMenu.add(newAction);
+         fileMenu.add(openAction);
+         fileMenu.add(cutAction);
+         fileMenu.add(copyAction);
+         fileMenu.add(pasteAction);
+         fileMenu.add(exitAction);
+         viewMenu.add(sizeAction);
+         viewMenu.add(browserAction);
+         controlMenu.add(controlActionStart);
+         controlMenu.add(controlActionPause);
+         dataMenu.add(dataActionStart);
+         dataMenu.add(dataActionPause);
+         helpMenu.add(helpAction);
     }
     
     private void setModelStatus(String status){
