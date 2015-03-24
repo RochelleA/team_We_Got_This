c778cd5d38295266e8262a94342f91fc7231a6d1
nur-magomed <nur-magomed@mail.ru>
2015-03-24 21:34:44 +0000

    Testing grid controller

--- Software/Final model/Java_SimulationEngine/src/tests/GridControllerTest.java
+++ Software/Final model/Java_SimulationEngine/src/tests/GridControllerTest.java
@@ -1,16 +1,16 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import model.GridController;
 import model.Model;
 
 import org.junit.Test;
 
 import core.Car;
-import core.Cell;
+import core.Direction;
 import core.Grid;
 import core.ICar;
-import core.ICell;
 import core.IGrid;
 
 public class GridControllerTest {
@@ -30,15 +30,183 @@ public class GridControllerTest {
 		car.setSpeed(1);
 		
 		gc.addCar(car);
-		int x = car.getX();
-		while(x>0){
+		int y = car.getY();
+		while(y>0){
 			gc.driveNorth(car);
-			x--;
-			assertEquals(car.getY(), x);
+			y--;
+			assertEquals(car.getY(), y);
 		}
 		
 	}
 	
+	@Test
+	public void testDriveSouth() {
+		IGrid grid = new Grid(10, 10);
+		GridController gc = new GridController(grid, new Model());
+		
+		ICar car = new Car();
+		car.setPosition(5, 5);
+		car.setSpeed(1);
+		
+		gc.addCar(car);
+		int y = car.getY();
+		while(y<9){
+			gc.driveSouth(car);
+			y++;
+			assertEquals(car.getY(), y);
+		}
+		
+	}
+	
+	@Test
+	public void testDriveWest() {
+		IGrid grid = new Grid(10, 10);
+		GridController gc = new GridController(grid, new Model());
+		
+		ICar car = new Car();
+		car.setPosition(5, 5);
+		car.setSpeed(1);
+		
+		gc.addCar(car);
+		int x = car.getX();
+		while(x>0){
+			gc.driveWest(car);
+			x--;
+			assertEquals(car.getX(), x);
+		}
+		
+	}
+	
+	@Test
+	public void testDriveEast() {
+		IGrid grid = new Grid(10, 10);
+		GridController gc = new GridController(grid, new Model());
+		
+		ICar car = new Car();
+		car.setPosition(5, 5);
+		car.setSpeed(1);
+		
+		gc.addCar(car);
+		int x = car.getX();
+		while(x<9){
+			gc.driveEast(car);
+			x++;
+			assertEquals(car.getX(), x);
+		}
+		
+	}
+	
+	@Test
+	public void testDriveNorthEast() {
+		IGrid grid = new Grid(10, 10);
+		GridController gc = new GridController(grid, new Model());
+		
+		ICar car = new Car();
+		car.setPosition(5, 5);
+		car.setSpeed(1);
+		
+		gc.addCar(car);
+		int x = car.getX();
+		int y = car.getY();
+		while(x<9 && y>0){
+			gc.driveNorthEast(car);
+			x++;
+			y--;
+			assertEquals(car.getX(), x);
+			assertEquals(car.getY(), y);
+		}
+		}
+		
+		@Test
+		public void testDriveEastSouth() {
+			IGrid grid = new Grid(10, 10);
+			GridController gc = new GridController(grid, new Model());
+			
+			ICar car = new Car();
+			car.setPosition(5, 5);
+			car.setSpeed(1);
+			
+			gc.addCar(car);
+			int x = car.getX();
+			int y = car.getY();
+			while(x<9 && y<9){
+				gc.driveEastSouth(car);
+				x++;
+				y++;
+				assertEquals(car.getX(), x);
+				assertEquals(car.getY(), y);
+			
+			}
+		
+		}
+		@Test
+		public void testDriveSouthWest() {
+			IGrid grid = new Grid(10, 10);
+			GridController gc = new GridController(grid, new Model());
+			
+			ICar car = new Car();
+			car.setPosition(5, 5);
+			car.setSpeed(1);
+			
+			gc.addCar(car);
+			int x = car.getX();
+			int y = car.getY();
+			while(x>0 && y<9){
+				gc.driveSouthWest(car);
+				x--;
+				y++;
+				assertEquals(car.getX(), x);
+				assertEquals(car.getY(), y);
+			
+			}
+		
+		}
+		@Test
+		public void testDriveWestNorth() {
+			IGrid grid = new Grid(10, 10);
+			GridController gc = new GridController(grid, new Model());
+			
+			ICar car = new Car();
+			car.setPosition(5, 5);
+			car.setSpeed(1);
+			
+			gc.addCar(car);
+			int x = car.getX();
+			int y = car.getY();
+			while(x>0 && y>0){
+				gc.driveWestNorth(car);
+				x--;
+				y--;
+				assertEquals(car.getX(), x);
+				assertEquals(car.getY(), y);
+			
+			}
+		
+		}
+		
+		@Test
+		public void testOverTake() {
+			IGrid grid = new Grid(10, 10);
+			GridController gc = new GridController(grid, new Model());
+			
+			ICar car = new Car();
+			car.setPosition(5, 5);
+			car.setSpeed(1);
+			
+			grid.placeCarAt(6, 5, car);
+			grid.setCellDirection(5, 5, Direction.EAST);
+			grid.setCellDirection(6, 5, Direction.EAST);
+			grid.setCellDirection(5, 6, Direction.EAST);
+			grid.setCellDirection(6, 6, Direction.EAST);
+			gc.addCar(car);
+			
+				gc.driveEast(car);
+
+				assertEquals(6, car.getX());
+				assertEquals(6, car.getY());
+		}
+		
+
 	@Test(expected = IndexOutOfBoundsException.class)
 	public void testDriveNorthOutOfBounds(){
 		IGrid grid = new Grid(10, 10);
@@ -53,6 +221,6 @@ public class GridControllerTest {
 		gc.driveNorth(car);
 		
 		//assertEquals(car.getY(), 0);
-	}
+		}
 
 }
